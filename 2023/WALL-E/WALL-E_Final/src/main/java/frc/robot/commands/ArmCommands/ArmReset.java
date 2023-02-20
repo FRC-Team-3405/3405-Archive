06cce11bb4d25dea1d90e266708b8c47f6c53929
sammer2605 <samuel.w139@stu.nebo.edu>
2023-02-20 15:34:01 -0700

    Add ArmReset InstantCommand

---
+++ 2023/WALL-E/WALL-E_Final/src/main/java/frc/robot/commands/ArmCommands/ArmReset.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.ArmCommands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.AC;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ArmReset extends InstantCommand {
+  public ArmReset() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.m_arm);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    RobotContainer.m_arm.setRotatePosition(AC.DEF_ROT);
+    RobotContainer.m_arm.setExtendPosition(AC.DEF_EXT);
+    System.out.println("The arm has been reset.");
+  }
+}
